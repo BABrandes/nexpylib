181ecc18bb658c6781ca08ada6996e777b03b82b
bab <benedikt.brandes@me.com>
2025-10-21 14:28:42 +0200

    minor version bump

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Your Name'
 author = 'Your Name'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 
